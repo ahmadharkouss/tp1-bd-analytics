56dfe1561327e654526c91a43c6aa557c0c07fc8
ahmad.harkous <ahmad.harkous@epita.fr>
2024-02-27 14:23:18 +0000

    ex1 : 70% done

---
+++ ex1/word_occ_count.py
@@ -0,0 +1,24 @@
+from mrjob import job
+
+class TP2(job.MRJob):
+    def mapper(self, _, line):
+        # Split the line into words
+        words = line.split()
+        # Emit each word with a count of 1
+        for word in words:
+            yield word, 1
+
+    def reducer(self, word, counts):
+        # Sum the counts for each word
+        yield word, sum(counts)
+
+
+
+if __name__ == '__main__':
+    #run with a file as input and  output to a file
+    TP2.run()
+
+    
+
+
+
